87824ebc9274fe9e43dcc69c1314ed3f1eac1a3d
Britni Chavis <brit.chavis@gmail.com>
2018-01-16 01:19:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if(string.toLowerCase() === string)
 else if (string.toUpperCase() === string)
 return "YES INDEED!"
 else if (string === string)
-return "I love you, too"
+return "I love you, too."
 }
 
 

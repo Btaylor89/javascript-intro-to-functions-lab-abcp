125da0fa41df0cf0ee59b7263d98238dac97c43f
Britni Chavis <brit.chavis@gmail.com>
2018-01-16 01:16:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
 if(string.toLowerCase() === string)
  return "I can't hear you!"
-else if (string.toUpperCase() === string) 
+else if (string.toUpperCase() === string)
 return "YES INDEED!"
 else if (string === string)
 return "I love you, too"
 }
  
-   
-   
+
+
  }
 
 
